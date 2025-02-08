e3326feca33e9f1e4908d4e3da23d4fad2888b41
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2025-02-08 14:32:19 -0800

    Set up app.js backend server

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,35 @@ const url = require('url');
 
 class DictionaryServer{
     constructor(port){
+        // Dictionary should be an array of objects
+        // -> {word: defintion}
+        this.dictionary = [];
         this.port = port;
-        this.server = http.createServer(this.handleReq)
+        this.server = http.createServer(this.handleRequest.bind(this));
+    }
+
+    handleRequest(req, res){
+        const parsedUrl = url.parse(req.url, true);
+        const pathname = parsedUrl.pathname;
+
+        if(req.method === 'GET'){
+            //Do stuff here
+            // get word def or return error message
+        }
+        else if(req.method === 'POST'){
+            // check if word in storage otherwise add and return success
+        }
+        else{
+            this.handleMethodNotAllowed(res);
+        }
+    }
+
+    /*
+    *   For Unallowed methods/requests
+    */
+    handleMethodNotAllowed(res) {
+        res.writeHead(405, { 'Content-Type': 'text/plain' });
+        res.end("Method Not Allowed");
     }
 }
 
